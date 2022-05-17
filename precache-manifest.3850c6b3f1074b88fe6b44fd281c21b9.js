d3201060011444edb53c9b4f6877d0d423639226
DubrovskayaDaya <dashadubrovskaya1999@gmail.com>
2022-05-17 18:44:27 +0300

    Updates

---
+++ precache-manifest.3850c6b3f1074b88fe6b44fd281c21b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f1ec3a6349d0fbe38af63112075108",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "761c06d9d0b71d7cc3bd",
+    "url": "/ToDoList/static/css/main.ad8241e1.chunk.css"
+  },
+  {
+    "revision": "4099e70309068f20a201",
+    "url": "/ToDoList/static/js/2.06a57156.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/ToDoList/static/js/2.06a57156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761c06d9d0b71d7cc3bd",
+    "url": "/ToDoList/static/js/main.5b6bc277.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
